03cece7d32582abf0f78251a884db2b9a57d3d95
inko81 <tadanosuzumetoinko@gmail.com>
2018-08-26 03:49:55 +0900

    ファイル名変更

--- src/main/java/com/example/demo/GeorgeCalc.java
+++ src/main/java/com/example/demo/GeorgeCalc.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalcApplication {
+public class GeorgeCalc {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalcApplication.class, args);
+		SpringApplication.run(GeorgeCalc.class, args);
 	}
 }
--- src/test/java/com/example/demo/GeorgeCalcTests.java
+++ src/test/java/com/example/demo/GeorgeCalcTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CalcApplicationTests {
+public class GeorgeCalcTests {
 
 	@Test
 	public void contextLoads() {
